670d4f1363f4a8b163ceb7da3f2d85dea9a1ecc9
yevhenip <yevhenip@gmail.com>
2022-01-29 16:42:56 +0200

    ITF-01-03 - Init chat project

---
+++ server/Services/Chat/Chat.Domain/ChatRoom.cs
@@ -0,0 +1,9 @@
+﻿namespace Chat.Domain;
+
+public class ChatRoom
+{
+    public Guid Id { get; set; }
+    public Guid RecruiterId { get; set; }
+    public Guid ApplicantId { get; set; }
+    public ICollection<Message> Messages { get; set; } = new List<Message>();
+}
---
+++ server/Services/Chat/Chat.Domain/Message.cs
@@ -0,0 +1,12 @@
+﻿namespace Chat.Domain;
+
+public class Message
+{
+    public Guid Id { get; set; }
+    public Guid ChatId { get; set; }
+    public Guid SenderId { get; set; }
+    public ChatRoom Chat { get; set; }
+    public DateTime Sent { get; set; }
+    public string Content { get; set; }
+    public bool Seen { get; set; }
+}
