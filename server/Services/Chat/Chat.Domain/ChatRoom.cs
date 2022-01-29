a747ce4ebc9cb1cb6548a7d75edc14a6e8add9c9
yevhenip <yevhenip@gmail.com>
2022-01-29 18:08:32 +0200

    ITF-01-03 - Pull request changes

--- server/Services/Chat/Chat.Domain/ChatRoom.cs
+++ server/Services/Chat/Chat.Domain/ChatRoom.cs
@@ -4,6 +4,6 @@ public class ChatRoom
 {
     public Guid Id { get; set; }
     public Guid RecruiterId { get; set; }
-    public Guid ApplicantId { get; set; }
+    public Guid DeveloperId { get; set; }
     public ICollection<Message> Messages { get; set; } = new List<Message>();
 }
